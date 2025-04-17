17b6a86170481e2c35a410e2a3fa7c46af0b278d
xatayuda <djayudax@gmail.com>
2025-04-17 10:16:33 -0400

    Update script.js
    Adding colors

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const colores = [
     "#801315",  // Color Guinda
     "#CAA100",  // Color Dorado
     "#010101",  // Color Negro
+    "#F08080",  // Color Rosa
+    "#9370DB",  // Color Morado Claro
+    "#3CB371",  // Color Verde Menta
+    "#87CEFA",  // Color Celeste Claro
+    "#0000CD",  // Color Azul Oscuro
+    "#FFA500",  // Color Anaranjado
+    "#FF4B45",  // Color Rojizo
+    "#FFC18F",  // Color Beige
 ];
     let indiceColor = 0;
 
